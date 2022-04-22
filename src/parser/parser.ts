6e3f6c3197d4888e1ba339fd07505a98d3bfc5d6
Eduin Garcia Cordero <eduinlight@gmail.com>
2022-04-22 17:36:54 +0300

    a little better

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -11,23 +11,17 @@ export class Parser {
         const firstParams = line.substring(0, jsonStart - separator.length)
         const jsonParam = line.substring(jsonStart)
         const lineValues = firstParams.split(separator)
+
         if (lineValues.length !== lineFormat.length - 1) {
           return reject(new LineFormatSettingException())
         }
 
-        const newFirstLineFormat = []
-        for (let i = 0; i < lineFormat.length - 1; i++) {
-          newFirstLineFormat.push(lineFormat[i])
-        }
-        const firsParamsObject =
-          newFirstLineFormat.map((parser, index) => parser.parse(lineValues[index]))
-            .reduce((acc, curr) => ({ ...acc, ...curr }), {})
-
+        const newFirstLineFormat = lineFormat.filter((_v, index) => index !== lineFormat.length - 1)
         response.push(
-          {
-            ...firsParamsObject,
-            ...lineFormat[lineFormat.length - 1].parse(jsonParam)
-          }
+          newFirstLineFormat.map((parser, index) => parser.parse(lineValues[index]))
+            .reduce((acc, curr) => ({ ...acc, ...curr }),
+              lineFormat[lineFormat.length - 1].parse(jsonParam)
+            )
         )
       }
       resolve(response)
