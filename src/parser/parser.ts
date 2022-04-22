f36923bfc145d7f0a8dc8779256900b658047104
Eduin Garcia Cordero <eduinlight@gmail.com>
2022-04-22 17:29:55 +0300

    changes done in the interview

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -7,15 +7,27 @@ export class Parser {
     return new Promise((resolve, reject) => {
       const response = []
       for (const line of lines) {
-        const lineValues = line.split(separator)
-
-        if (lineValues.length !== lineFormat.length) {
+        const jsonStart = line.search('{')
+        const firstParams = line.substring(0, jsonStart - separator.length)
+        const jsonParam = line.substring(jsonStart)
+        const lineValues = firstParams.split(separator)
+        if (lineValues.length !== lineFormat.length - 1) {
           return reject(new LineFormatSettingException())
         }
 
-        response.push(
-          lineFormat.map((parser, index) => parser.parse(lineValues[index]))
+        const newFirstLineFormat = []
+        for (let i = 0; i < lineFormat.length - 1; i++) {
+          newFirstLineFormat.push(lineFormat[i])
+        }
+        const firsParamsObject =
+          newFirstLineFormat.map((parser, index) => parser.parse(lineValues[index]))
             .reduce((acc, curr) => ({ ...acc, ...curr }), {})
+
+        response.push(
+          {
+            ...firsParamsObject,
+            ...lineFormat[lineFormat.length - 1].parse(jsonParam)
+          }
         )
       }
       resolve(response)
